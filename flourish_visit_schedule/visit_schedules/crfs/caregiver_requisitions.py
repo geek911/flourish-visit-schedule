db39d14cd32929dc1b4acb4c589420ea0730d9c3
Itumeleng Mosweu <imosweuk@gmail.com>
2021-10-18 11:14:06 +0200

    update caregiver requisitions

---
+++ flourish_visit_schedule/visit_schedules/crfs/caregiver_requisitions.py
@@ -0,0 +1,24 @@
+from edc_visit_schedule import FormsCollection, Requisition
+from flourish_labs import viral_load_panel, cd4_panel, hematology_panel, cbc_panel
+
+requisitions_prn = FormsCollection(
+    Requisition(
+        show_order=10,
+        panel=cd4_panel, required=False, additional=False),
+    Requisition(
+        show_order=20,
+        panel=viral_load_panel, required=False, additional=False),
+    Requisition(
+        show_order=30,
+        panel=hematology_panel, required=False, additional=False),
+    Requisition(
+        show_order=40,
+        panel=cbc_panel, required=False, additional=False),
+    name='requisitions_prn')
+
+# Main Study Requisitions
+preg_requisitions = FormsCollection(
+    Requisition(
+        show_order=10,
+        panel=viral_load_panel, required=True, additional=False),
+    )
