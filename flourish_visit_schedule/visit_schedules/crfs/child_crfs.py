d2b0ffb57ee0c2999322fd4ad5ced99b82958281
Moses Chawawa <moseschawawa@gmail.com>
2022-12-16 09:45:15 +0200

    changed order

--- flourish_visit_schedule/visit_schedules/crfs/child_crfs.py
+++ flourish_visit_schedule/visit_schedules/crfs/child_crfs.py
@@ -198,8 +198,8 @@ tb_adol_enrollment = FormsCollection(
     Crf(show_order=4, model='flourish_child.tbhistoryadol'),
     Crf(show_order=5, model='flourish_child.tbroutinescreenadol'),
     Crf(show_order=6, model='flourish_child.covid19adol', required=False),
-    Crf(show_order=7, model='flourish_child.tbpresencehouseholdmembersadol'),
-    Crf(show_order=8, model='flourish_child.tbvisitscreeningadolescent'),
+    Crf(show_order=7, model='flourish_child.tbvisitscreeningadolescent'),
+    Crf(show_order=8, model='flourish_child.tbpresencehouseholdmembersadol'),
     Crf(show_order=9, model='flourish_child.hivtestingadol'),
     Crf(show_order=10, model='flourish_child.tblabresultsadol'),
     name='tb_adol_enrollement'
